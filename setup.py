daf5f106de13ae54cecdd5728f7bdbe4859a6a89
Pourush Kumar Gupta <pkg.21p10161@mtech.nitdgp.ac.in>
2023-08-10 17:02:35 +0530

    file added

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ PACKAGES=["housing"]
 REQUIREMENT_FILE_NAME="requirements.txt"
 HYPHEN_E_DOT = "-e ."
 
-def get_requirements_list():
+def get_requirements_list()->List[str]:
     """
     Description: This function is going to return list of requirement mention
     in requirement.txt file
