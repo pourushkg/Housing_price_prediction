7d1bf4d707ab86118b9d5e9ad1b489843b9cb8de
Pourush Kumar Gupta <pkg.21p10161@mtech.nitdgp.ac.in>
2023-08-10 16:46:45 +0530

    updated

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ from typing import List
 
 # Declearing variable for setup functions
 PROJECT_NAME="housing_predictor"
-VERSION="0.0.1"
+VERSION="0.0.2"
 AUTHOR="pourush kumar gupta"
 DESCRIPTION="This is my first machine learning project"
 PACKAGES=["housing"]
 REQUIREMENT_FILE_NAME="requirements.txt"
+HYPHEN_E_DOT = "-e ."
 
 def get_requirements_list():
     """
@@ -19,9 +20,11 @@ def get_requirements_list():
     requirement.txt file
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
-    
-
+        requirement_list = requirement_file.readlines()
+        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+        return requirement_list
 
 setup(
     name=PROJECT_NAME,
